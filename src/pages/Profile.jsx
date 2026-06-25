60ace0423cb921c2b6813cd1a72a13c5a035ec6c
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 06:41:44 +0600

    Create Profile component with user details and stats

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,54 @@
+export default function Profile(){
+
+
+return(
+
+<div className="profile">
+
+
+<img 
+src="https://picsum.photos/200"
+/>
+
+
+<h1>
+ALVI AHMED
+</h1>
+
+
+<p>
+@Real_Alvi
+</p>
+
+
+<div className="stats">
+
+<div>
+0
+<br/>
+Played
+</div>
+
+
+<div>
+0
+<br/>
+Liked
+</div>
+
+
+<div>
+0
+<br/>
+Downloads
+</div>
+
+
+</div>
+
+
+</div>
+
+)
+
+}
