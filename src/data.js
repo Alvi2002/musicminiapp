4f1d7cac790bfbbb1fcf712b38a1c7c0c07ca2c6
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 06:46:25 +0600

    Add songs data to src/data.js

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const songs = [
+
+{
+id:1,
+title:"Do You Remember",
+artist:"Queen",
+image:"https://picsum.photos/300/300",
+audio:"#"
+},
+
+
+{
+id:2,
+title:"Top Songs 2024",
+artist:"Love Life",
+image:"https://picsum.photos/301/300",
+audio:"#"
+},
+
+
+{
+id:3,
+title:"Romantic Acoustic",
+artist:"Acoustic Pulse",
+image:"https://picsum.photos/302/300",
+audio:"#"
+}
+
+];
