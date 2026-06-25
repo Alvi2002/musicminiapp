4a39920636533406f8e904b24b12b1fdf62fb7c7
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 07:08:38 +0600

    Fetch and display Telegram user profile data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,38 @@
+import {
+useEffect,
+useState
+}
+from "react";
+
+
+import {
+getTelegramUser
+}
+from "../telegram/telegram";
+
+
+
 export default function Profile(){
+
+
+const [user,setUser]=useState(null);
+
+
+
+useEffect(()=>{
+
+
+const data =
+getTelegramUser();
+
+
+setUser(data);
+
+
+
+},[]);
+
+
 
 
 return(
@@ -6,49 +40,109 @@ return(
 <div className="profile">
 
 
-<img 
-src="https://picsum.photos/200"
+<img
+
+src={
+user?.photo_url ||
+"https://picsum.photos/200"
+}
+
 />
 
 
+
 <h1>
-ALVI AHMED
+
+{
+
+user?.first_name ||
+
+"Telegram User"
+
+}
+
+
 </h1>
 
 
+
 <p>
-@Real_Alvi
+
+@
+
+{
+
+user?.username ||
+
+"unknown"
+
+}
+
 </p>
+
+
+
+<p>
+
+ID:
+
+{
+
+user?.id ||
+
+"00000"
+
+}
+
+</p>
+
 
 
 <div className="stats">
 
+
 <div>
+
 0
+
 <br/>
+
 Played
+
 </div>
 
 
 <div>
+
 0
+
 <br/>
+
 Liked
+
 </div>
+
 
 
 <div>
+
 0
+
 <br/>
+
 Downloads
+
 </div>
 
 
+
 </div>
+
 
 
 </div>
 
 )
+
 
 }
