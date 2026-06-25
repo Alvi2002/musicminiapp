7692f008efb2e2dc7c050201fd46b8fcfef582d6
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 07:12:02 +0600

    Enhance Home component with search and UI updates
    Refactor Home component to improve song filtering and UI elements.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,90 +1,155 @@
+import { useState } from "react";
+
 import MusicCard from "../components/MusicCard";
+import Search from "../components/Search";
 
-import {songs} from "../data";
-
+import { songs } from "../data";
 
 
 export default function Home({
 
-setPlaylist,
-playlist,
-setSong
+  setPlaylist,
+  playlist,
+  setSong
 
 }){
+
+
+const [search,setSearch] = useState("");
+
+
+
+const filteredSongs = songs.filter(song =>
+
+song.title
+.toLowerCase()
+.includes(
+search.toLowerCase()
+)
+
+||
+song.artist
+.toLowerCase()
+.includes(
+search.toLowerCase()
+)
+
+);
 
 
 
 function addSong(song){
 
 
-if(!playlist.find(
-x=>x.id===song.id
-)){
+const alreadyAdded = playlist.find(
+item => item.id === song.id
+);
+
+
+
+if(!alreadyAdded){
 
 
 setPlaylist([
+
 ...playlist,
+
 song
 
-])
+]);
 
 
 }
 
-
 }
-
 
 
 
 return(
 
+
 <div className="home">
 
 
+<div className="header">
+
+
 <h1>
-Good Morning
+Good Morning 👋
 </h1>
 
 
-<div className="search">
+<p>
+Listen your favourite music
+</p>
 
-🔍 Search music
 
 </div>
 
 
 
+<Search
+
+value={search}
+
+setValue={setSearch}
+
+/>
+
+
+
 <h2>
-Popular Now
+Popular Now 🎵
 </h2>
 
 
 
 {
 
-songs.map(song=>(
+filteredSongs.length > 0 ?
+
+
+filteredSongs.map(song => (
+
 
 <MusicCard
 
+
 key={song.id}
+
 
 song={song}
 
+
 addSong={addSong}
 
+
 playSong={setSong}
+
+
 
 />
 
 
 ))
 
+
+:
+
+
+<p className="empty">
+
+No song found
+
+</p>
+
+
 }
 
 
 
 </div>
+
 
 )
 
