36f2dba2d868d9e2ac927b5ae8075f09029d94da
Alvi2002 <hasibsheikh400@gmail.com>
2026-06-25 06:30:59 +0600

    Create App component with Home and BottomNav

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import BottomNav from "./components/BottomNav";
+import Home from "./pages/Home";
+
+function App(){
+
+return(
+<div>
+
+<Home/>
+
+<BottomNav/>
+
+</div>
+)
+
+}
+
+export default App;
